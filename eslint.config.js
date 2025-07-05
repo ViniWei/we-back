d64bf716ae126cb6bd12162086d704edc79be6da
ViniWei <viniciusweigert152@gmail.com>
2025-07-05 16:50:38 -0300

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+    { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
+    {
+        rules: {
+            "no-unreachable": "error",
+            "no-unused-expressions": "error",
+            "no-unused-vars": "error",
+            "prefer-const": "error",
+            "indent": ["error", 4],
+            "semi": ["error"],
+        },
+    },
+]);
