f48611c6c1b28312c2528673e85b081bc16a01f5
Jorge Willian Paez Nagakura <jorgewillianpaez@gmail.com>
2025-08-15 17:29:57 -0300

    Creating getAllByField method within BaseRepository

--- repository/movieLists.repository.js
+++ repository/movieLists.repository.js
@@ -3,19 +3,14 @@ import BaseRepository from "./BaseRepository.js";
 const tableName = "movie_lists";
 const baseRepository = new BaseRepository(tableName);
 
+const getListsByCouple = async(coupleId) => { return await baseRepository.getAllByField("couple_id", coupleId); };
 const getById = async(id) => { return await baseRepository.getFirstByField("id", id); };
 const create = async(movie) => { return await baseRepository.create(movie); };
 const deleteList = async(listId) => { return await baseRepository.deleteAllByField("id", listId); };
 
-const getListsByCouple = async(coupleId) => { 
-    const result = await pool.query("SELECT * FROM movie_lists WHERE couple_id = ?", [coupleId]);
-
-    return result[0];
-};
-
 export default {
+    getListsByCouple,
     getById,
     create,
-    getListsByCouple,
     deleteList
 };
