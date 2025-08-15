fdd2310e494b3495adcc774097f7a7cebe7e1a3c
Jorge Willian Paez Nagakura <jorgewillianpaez@gmail.com>
2025-08-15 15:12:54 -0300

    Fixing deleList method within movieLists repository

--- repository/movieLists.repository.js
+++ repository/movieLists.repository.js
@@ -5,15 +5,12 @@ const baseRepository = new BaseRepository(tableName);
 
 const getById = async(id) => { return await baseRepository.getFirstByField("id", id); };
 const create = async(movie) => { return await baseRepository.create(movie); };
+const deleteList = async(listId) => { return await baseRepository.deleteAllByField("id", listId); };
 
 const getListsByCouple = async(coupleId) => { 
     const result = await pool.query("SELECT * FROM movie_lists WHERE couple_id = ?", [coupleId]);
 
     return result[0];
-};
-
-const deleteList = async(listId) => {
-    await baseRepository.deleteAllByField("id", listId);
 };
 
 export default {
