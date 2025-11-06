bd66908705dd7f03798d297be9c58bbf956ebdcd
Jorge Nagakura <jorge.nagakura@nexergroup.com>
2025-11-06 19:34:16 -0300

    add: alterando para o envio de e-mails utilizar ssl

--- src/services/mailer.service.ts
+++ src/services/mailer.service.ts
@@ -5,7 +5,7 @@ import fs from "fs/promises";
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: false,
+  secure: true,
   auth: {
     user: process.env.SMTP_AUTH_USER,
     pass: process.env.SMTP_AUTH_PASS,
